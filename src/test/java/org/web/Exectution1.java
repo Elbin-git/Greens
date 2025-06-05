19ede2a2f182561859512c79bf0194b5635b44f9
Elbin.git <itselbin95@gmail.com>
2025-06-05 14:04:15 +0530

    git has been installed

---
+++ src/test/java/org/web/Exectution1.java
@@ -0,0 +1,74 @@
+package org.web;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Exectution1 {
+	@BeforeSuite
+	private void methBSuite() {
+		System.out.println("E1 Before Suite"+System.currentTimeMillis());
+	}
+	@AfterSuite
+	private void methASuite() {
+	System.out.println("E1 After Suite");
+	}
+	@BeforeTest
+	private void methBTest() {
+	   System.out.println("E1 Before Test"+System.currentTimeMillis());
+	}
+	@AfterTest
+	private void methATest() {
+		System.out.println("E1 After Test"+System.currentTimeMillis());
+	}
+	@BeforeClass
+	private void methBClass() {
+		System.out.println("E1 Before class"+System.currentTimeMillis());
+		}
+	@AfterClass
+	private void methAClass() {
+		System.out.println("E1 After class"+System.currentTimeMillis());
+	}
+	@BeforeMethod()
+	private void methBMethod() {
+		System.out.println("E1 Before Method"+System.currentTimeMillis());
+	}
+	@AfterMethod
+	private void methAMethod() {
+		System.out.println("E1 After Method"+System.currentTimeMillis());
+	}
+	@Test(priority=-3)
+	private void meth3() {
+		System.out.println("E1 Print : 3"+System.currentTimeMillis());
+	}
+	@Test(invocationCount=4)
+	private void meth2() {
+		System.out.println("E1 Print : 2"+System.currentTimeMillis());
+	}
+	@Test
+	private void meth1() {
+		System.out.println("E1 Print : 1"+System.currentTimeMillis());
+	}
+	}
+
+		
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+
+
---
+++ src/test/java/org/web/Execution2.java
@@ -0,0 +1,58 @@
+package org.web;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Execution2 {
+@BeforeSuite
+private void methBSuite() {
+	System.out.println("Before Suite"+System.currentTimeMillis());
+}
+@AfterSuite
+private void methASuite() {
+System.out.println("After Suite"+System.currentTimeMillis());
+}
+@BeforeTest
+private void methBTest() {
+   System.out.println("E2 Before Test"+System.currentTimeMillis());
+}
+@AfterTest
+private void methATest() {
+	System.out.println("E2 After Test"+System.currentTimeMillis());
+}
+@BeforeClass
+private void methBClass() {
+	System.out.println("E2 Before class"+System.currentTimeMillis());
+	}
+@AfterClass
+private void methAClass() {
+	System.out.println("E2 After class"+System.currentTimeMillis());
+}
+@BeforeMethod
+private void methBMethod() {
+	System.out.println("E2 Before Method"+System.currentTimeMillis());
+}
+@AfterMethod
+private void methAMethod() {
+	System.out.println("E2 After Method"+System.currentTimeMillis());
+}
+@Test(priority=-3)
+private void meth3() {
+	System.out.println("E2 Print : 3"+System.currentTimeMillis());
+}
+@Test(invocationCount=4)
+private void meth2() {
+	System.out.println("E2 Print : 2"+System.currentTimeMillis());
+}
+@Test
+private void meth1() {
+	System.out.println("E2 Print : 1"+System.currentTimeMillis());
+}
+}
